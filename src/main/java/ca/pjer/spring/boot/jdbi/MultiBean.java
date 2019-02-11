74be0dcd0d0bff21308398831f5db34f633e3dd0
Pierre-David Belanger <pierredavidbelanger@gmail.com>
2019-02-10 21:29:57 -0500

    typeSet to iterate multibean types

--- src/main/java/ca/pjer/spring/boot/jdbi/MultiBean.java
+++ src/main/java/ca/pjer/spring/boot/jdbi/MultiBean.java
@@ -3,12 +3,17 @@ package ca.pjer.spring.boot.jdbi;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MultiBean implements Cloneable, Serializable {
     private final Map<Class, Object> beans = new HashMap<>();
 
     void put(Class type, Object bean) {
         beans.put(type, bean);
+    }
+
+    public Set<Class> typeSet() {
+        return beans.keySet();
     }
 
     public <T> T get(Class<T> type) {
